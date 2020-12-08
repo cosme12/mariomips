1e5ec859d57c3a8e277fb85265e5c8370968cf40
cosme12 <aaadiego502@gmail.com>
2020-12-08 00:25:31 -0300

    Primera prueba sprite multicolor

--- mips1.s
+++ mips1.s
@@ -4,7 +4,7 @@ CONTROL: .word32 0x10000
 DATA: .word32 0x10008
 
 
-COLORES: .word32 0x000000, 0xe8f1ff, 0x532b1d, 0x82769c , 0x4d00ff ,0xa877ff , 0xaaccff, 0x36e400, 0x518700, 0x3652ab, 0x27ecff
+COLORES: .word32 0x000000, 0xe8f1ff, 0x532b1d, 0x82769c , 0x7e2553, 0x4d00ff ,0xa877ff , 0xaaccff, 0x36e400, 0x518700, 0x3652ab, 0x27ecff
 
 
 ARROW_DER: .byte 0x64
@@ -13,8 +13,8 @@ ARROW_ARR: .byte 32
 ARROW_ABJ: .byte 32
 
 
-BACKGROUND: .byte 12,0,0,0,1,0,2,0,3,0,4,1,0,1,1,1,2,1,3,1,4,2,0,2,1,2,2,2,3,2,4,98,28,3,0,3,1,3,2,3,3,3,4,4,0,4,1,4,2,4,3,4,4,99
-
+BACKGROUND: .byte 12,0,0,1,0,2,0,3,0,4,0,0,1,1,1,2,1,3,1,4,1,0,2,1,2,2,2,3,2,4,2,0,3,1,3,2,3,3,3,4,3,0,4,1,4,2,4,3,4,4,4,99
+SUELO1: .byte 40,0,0,1,0,3,0,4,0,0,1,1,1,2,1,3,1,4,1,98,16,2,0,0,2,1,2,2,2,3,2,4,2,98,36,0,3,1,3,2,3,3,3,4,3,1,4,2,4,3,4,4,4,98,32,0,4,99
 
 ;Mapa
 
@@ -27,7 +27,7 @@ FILA6:	.byte  2,0,0,0,0,0,0,0,0,2
 FILA7:	.byte  2,0,0,0,0,0,0,0,0,2
 FILA8:	.byte  2,0,0,0,0,0,0,0,0,2
 FILA9:	.byte  2,0,0,0,0,0,0,0,0,2
-FILA10:	.byte  2,1,1,1,1,1,1,1,1,2
+FILA10:	.byte  1,1,1,1,1,1,1,1,1,1
 
 
 
@@ -67,12 +67,19 @@ dibujar:		beq $t0, $t1, finDibujar 			; si se dibujo toda la matriz, finalizar s
 				daddi $t6, $zero, 0					; condicion de sprite a dibujar 0 == BACKGROUND
 				daddi $t7, $zero, 1					; offset de pixeles del objeto a dibujar
 				beq $t5, $t6, dibujarBackgr			; $t5 == 0 => dibujar background
+				daddi $t6, $zero, 1					; condicion de sprite a dibujar 1 == SUELO1
+				beq $t5, $t6, dibujarSuelo1			; $t5 == 0 => dibujar suelo1
 				j finElemento
 
 cambiarColor:	daddi $t7, $t7, 1
 				dadd $t8, $zero, $t7				; $t8 guarda offset al nuevo color
 				daddi $t7, $t7, 1
 				j dibujarBackgr
+
+cambiarColor1:	daddi $t7, $t7, 1
+				dadd $t8, $zero, $t7				; $t8 guarda offset al nuevo color
+				daddi $t7, $t7, 1
+				j dibujarSuelo1
 
 dibujarBackgr:	lbu $t6, BACKGROUND($t7)			; carga la posX del sprite a dibujar
 				beq $t6, $t9, finElemento			; $t6 == 99 => pasar al siguiente sprite
@@ -96,6 +103,29 @@ dibujarBackgr:	lbu $t6, BACKGROUND($t7)			; carga la posX del sprite a dibujar
 				sd $t6, 0($s1)						; CONTROL recibe 5 y produce el dibujo del punto
 				
 				j dibujarBackgr
+
+dibujarSuelo1:	lbu $t6, SUELO1($t7)			; carga la posX del sprite a dibujar
+				beq $t6, $t9, finElemento			; $t6 == 99 => pasar al siguiente sprite
+				daddi $t9, $zero, 98				; 
+				beq $t6, $t9, cambiarColor1			; $t6 == 98 => cambiar de color
+				daddi $t9, $zero, 99				;
+				dadd $t6, $t6, $t2   				; ajusta posicion X del sprite
+				sb $t6, 5($s0)						; DATA+5 recibe el valor de coordenada X
+				daddi $t7, $t7, 1					; incrementa offset de pixeles del objeto a dibujar
+
+				lbu $t6, SUELO1($t7)			; carga la posY del sprite a dibujar
+				dadd $t6, $t6, $t3   				; ajusta posicion Y del sprite
+				sb $t6, 4($s0)						; DATA+4 recibe el valor de coordenada Y
+				daddi $t7, $t7, 1					; incrementa offset de pixeles del objeto a dibujar
+
+													; pintar y mostrar
+				lbu $t6, SUELO1($t8)  			; $t6 = offset, respecto a NEGRO, del color a pintar
+				lwu $t6, COLORES($t6)				; $t6 = color a pintar
+				sw $t6, 0($s0)						; DATA recibe el valor del color a pintar
+				daddi $t6, $zero, 5					; $t0 = 5 -> función 5: salida gráfica
+				sd $t6, 0($s1)						; CONTROL recibe 5 y produce el dibujo del punto
+				
+				j dibujarSuelo1
 
 finElemento:	daddi $t0, $t0, 1					; pasa al siguiente elemento de la matriz
 				daddi $t2, $t2, 5					; cambia la coordenada X
